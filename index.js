2e688f1c46fece1ce898b35b375080784276f00c
j0ej8ck92 <j0ej8ck92@gmail.com>
2026-08-01 18:58:56 +0000

    commit

---
+++ index.js
@@ -0,0 +1,81 @@
+//A capitalize function that takes a string and returns it with the first character capitalized.
+export function capitalize(str){
+  const manipulation = str.split('');
+  const change = str.charAt(0).toUpperCase() + str.slice(1);
+  return change;
+}
+
+//A reverseString function that takes a string and returns it reversed.
+export function reverse(str) {
+  const change = str.split('').reverse().join('');
+  return change;
+}
+
+//A reverseString function that takes a string and returns it reversed with higher performance.
+export function revPerform(str) {
+  let reversed = '';
+  for (i = str.length - 1; i >= 0; i--){
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+//A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply.
+//  Each of these functions should take two numbers and return the correct calculation.
+export const calc = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    if (b === 0) {
+      return 0;
+    } else {
+      return a / b;
+    }
+  } 
+};
+
+//A caesarCipher function that takes a string and a shift factor and returns it with each character “shifted”. 
+//For example, a = d, b = e, etc... 
+//Make sure the output is case sensitive along with punctuation as well.
+export function caesarCipher(str, shift){
+  return str.split('').map(char => {
+    const ASCII_dec = char.charCodeAt(0)
+
+    if (ASCII_dec > 96 && ASCII_dec < 123 ){
+      const result = ((ASCII_dec - 97 + shift) % 26) + 97;
+      return String.fromCharCode(result);
+    }
+    if (ASCII_dec > 64 && ASCII_dec < 91 ){
+      const result = ((ASCII_dec - 97 + shift) % 26) + 97;
+      return String.fromCharCode(result);
+    }
+    return char;
+  }).join('');
+}
+
+//An analyzeArray function that takes an array of numbers and returns an object 
+// with the following properties: average, min, max, and length.
+export function analyzeArray(arr){
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+    sum += arr[i];
+  }
+  const average = sum / arr.length;
+
+  return {
+    average: average,
+    min: min,
+    max: max,
+    length: arr.length
+  };
+
+}
+
+
+
+
---
+++ index.test.js
@@ -0,0 +1,37 @@
+import { capitalize, reverse, revPerform, calc, caesarCipher, analyzeArray } from "./index.js";
+
+test('capitalizes first letter of word',  () => {
+  expect(capitalize('function')).toMatch('Function');
+})
+
+test('reverses function string characters', () => {
+  expect(reverse('function')).toMatch('noitcnuf');
+})
+
+test('reverses function string characters', () => {
+  expect(revPerform('function')).toMatch('noitcnuf');
+})
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(calc.add(1,2)).toBe(3);
+});
+
+test('subtracts 10 - 5 to equal 5', () => {
+  expect(calc.subtract(10, 5)).toBe(5);
+});
+
+test('multiplies 10 * 10 to equal 100', () => {
+  expect(calc.multiply(10, 10)).toBe(100);
+});
+
+test('divides 30 / 5 to equal 6', () => {
+  expect(calc.divide(30, 5)).toBe(6);
+});
+
+test('returns string using Caesar Shift Cipher with a sting input of "Hello, World!" and shift int input of 3 & returns "Khoor, Zruog!"', () => { 
+  expect (caesarCipher('Hello, World!', 3)).toMatch('Khoor, Zruog!');
+});
+
+test('returns [1,8,3,4,2,6] with min, max, average, and lenth of array', () => {
+  expect (analyzeArray([1,8,3,4,2,6])).toEqual({average: 4, min: 1, max: 8, length: 6 })
+})
